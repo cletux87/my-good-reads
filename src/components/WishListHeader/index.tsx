f129441e0213e3a81330056696f0494fa9ca1a3b
Your Name <you@example.com>
2021-02-13 02:00:55 -0600

    Remove unncessary files

--- src/components/WishListHeader/index.tsx
+++ src/components/WishListHeader/index.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import CoinWithNumber from "./CoinWithNumber";
 import "./WishListHeaderStyles.scss";
 
 const WishListHeader = () => {
   const listOfBooks = useSelector((state: any) => state.booksReducer);
 
-  return ( //anidar coin with number
+  return ( 
     <div id="wishListHeader" className="wishListContainer">
       <label>My Reading Wish list</label>
       <div className="wishListNumber">
-        <CoinWithNumber value={listOfBooks.myFavorites.length} />
+        <div id="wishListState">{listOfBooks.myFavorites.length}</div>
       </div>
     </div>
   );
